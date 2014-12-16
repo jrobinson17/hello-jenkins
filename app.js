e5fe2c0393933059c7da2381d0f943100475b944
jay <robinson039@gmail.com>
2014-12-16 18:42:43 -0500

    hello

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello fworld');
-	res.send('MetLife Tests #1');
+  res.send('hello world');
+	//res.send('MetLife Tests #1');
 });
  
 app.listen(process.env.PORT || 5000);
